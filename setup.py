4c3c523bc264e1e5fce05c352ef834a80e2fd12f
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2023-07-15 11:19:54 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+# 
+
+setup(
+  name = 'hive',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Hive - Pytorch',
+  author = 'Kye Gomez',
+  author_email = 'kye@apac.ai',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/kyegomez/Hive',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'optimizers',
+    "Prompt Engineering"
+  ],
+  install_requires=[
+        'swarms',
+    ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
